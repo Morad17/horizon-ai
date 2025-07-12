3189bfdac82f1cbe87090588e64d5c3f8cc6d3bb
Morad17 <morad123@hotmail.co.uk>
2025-07-13 00:15:19 +0100

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,42 +8,29 @@ import opportunities from "../assets/icons/job.svg";
 import about from "../assets/icons/about.svg";
 
 const Navbar = () => {
-  const handleLinkClick = (e) => {
-    // Force immediate navigation
-    e.stopPropagation();
-  };
-
   return (
     <nav className="main-nav">
       <div className="nav-logo">LOGO</div>
       <ul className="main-nav-links">
         <li className="link">
           <img className="nav-icon" src={home} alt="" />
-          <Link to="/" onClick={handleLinkClick}>
-            Home
-          </Link>
+          <Link to="/">Home</Link>
         </li>
         <li className="dropdown d-about">
           <div className="dropdown-main">
             <div className="dropdown-title">
               <img className="nav-icon" src={about} alt="" />
-              <Link to="/about-us" onClick={handleLinkClick}>
-                About Us
-              </Link>
+              <Link to="/about-us">About Us</Link>
             </div>
 
             <TiArrowSortedDown className="dropdown-arrow" />
           </div>
           <ul className="dropdown-menu">
             <li className="dropdown-link">
-              <Link to="/about-us" onClick={handleLinkClick}>
-                About Us
-              </Link>
+              <Link to="/about-us">About Us</Link>
             </li>
             <li className="dropdown-link">
-              <Link to="/faq" onClick={handleLinkClick}>
-                FAQ's
-              </Link>
+              <Link to="/faq">FAQ's</Link>
             </li>
           </ul>
         </li>
@@ -51,47 +38,33 @@ const Navbar = () => {
           <div className="dropdown-main">
             <div className="dropdown-title">
               <img className="nav-icon" src={opportunities} alt="" />
-              <Link to="/opportunities" onClick={handleLinkClick}>
-                Opportunities
-              </Link>
+              <Link to="/opportunities">Opportunities</Link>
             </div>
 
             <TiArrowSortedDown className="dropdown-arrow" />
           </div>
           <ul className="dropdown-menu">
             <li className="dropdown-link">
-              <Link to="/clients" onClick={handleLinkClick}>
-                Clients
-              </Link>
+              <Link to="/clients">Clients</Link>
             </li>
             <li className="dropdown-link">
-              <Link to="/opportunities" onClick={handleLinkClick}>
-                Opportunities
-              </Link>
+              <Link to="/opportunities">Opportunities</Link>
             </li>
             <li className="dropdown-link">
-              <Link to="/careers" onClick={handleLinkClick}>
-                Careers
-              </Link>
+              <Link to="/careers">Careers</Link>
             </li>
             <li className="dropdown-link">
-              <Link to="/money-generated" onClick={handleLinkClick}>
-                Money Generated
-              </Link>
+              <Link to="/money-generated">Money Generated</Link>
             </li>
           </ul>
         </li>
         <li className="link">
           <img className="nav-icon" src={blog} alt="" />
-          <Link to="/blog" onClick={handleLinkClick}>
-            Blog
-          </Link>
+          <Link to="/blog">Blog</Link>
         </li>
         <li className="link">
           <img className="nav-icon" src={contact} alt="" />
-          <Link to="/contact-us" onClick={handleLinkClick}>
-            Contact Us
-          </Link>
+          <Link to="/contact-us">Contact Us</Link>
         </li>
       </ul>
     </nav>
